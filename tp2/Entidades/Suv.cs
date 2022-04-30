7a89f94015ce58d138d0076ccc0492692f8f9104
unknown <tecnimakinas@gmail.com>
2022-04-29 22:41:44 -0300

    viernes

---
+++ tp2/Entidades/Suv.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public sealed class Suv : Vehiculo
+    {
+        
+        /// <summary>
+        /// Constructor llama clase base para crear objeto
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Suv(EMarca marca, string chasis, ConsoleColor color):base(chasis, marca, color)
+        {            
+        }
+        /// <summary>
+        /// SUV son 'Grande'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+             get { return ETamanio.Grande; }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SUV");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine("---------------------");
+            return sb.ToString();
+        }
+    }
+}
